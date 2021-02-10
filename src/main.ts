a6d887a1d7804a977d8c534ae2ca375b8107dd8e
Andres Pulido <apcastillo@lemonway.com>
2021-02-10 17:03:53 +0100

    CORS add

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    const options = new DocumentBuilder()
+        .setTitle('Marketplace')
+        .setDescription('POC of Marketplace from the comply Sqad')
+        .setVersion('1.0')
+        .addTag('marketplace')
+        .build();
 
-  const options = new DocumentBuilder()
-    .setTitle('Marketplace')
-    .setDescription('POC of Marketplace from the comply Sqad')
-    .setVersion('1.0')
-    .addTag('marketplace')
-    .build();
-        
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('swagger', app, document);
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+    await app.listen(3000);
 }
 bootstrap();
